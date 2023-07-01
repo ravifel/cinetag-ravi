5d49f80ed108f0ef23524efe3db8b387a24d600d
Ravi Silva <ravifelipee@gmail.com>
2023-07-01 16:06:56 -0300

    implementação do componente de 'Banner'

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Cabecalho from 'components/Cabecalho'
 import Rodape from 'components/Rodape'
+import Banner from 'components/Banner'
 
 export default function Inicio() {
     return (
         <>
             <Cabecalho />
+            <Banner imagem="home" />
             <Rodape />
         </>
     )

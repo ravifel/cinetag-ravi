9d591276167cb11a0e8ed223724dcf0d158fb6c7
Ravi Silva <ravifelipee@gmail.com>
2023-07-01 16:24:40 -0300

    implementação do componente de 'Titulo'

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -2,12 +2,16 @@ import React from 'react'
 import Cabecalho from 'components/Cabecalho'
 import Rodape from 'components/Rodape'
 import Banner from 'components/Banner'
+import Titulo from 'components/Titulo'
 
 export default function Inicio() {
     return (
         <>
             <Cabecalho />
             <Banner imagem="home" />
+            <Titulo>
+                <h1>Um lugar para guardar seus vídeos e filmes!</h1>
+            </Titulo>
             <Rodape />
         </>
     )

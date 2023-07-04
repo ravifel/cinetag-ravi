2362f7983f6070912c921fdbe69b729e63bf4727
Ravi Silva <ravifelipee@gmail.com>
2023-07-03 23:17:06 -0300

    implementação do componente de Card'

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -3,6 +3,7 @@ import Cabecalho from 'components/Cabecalho'
 import Rodape from 'components/Rodape'
 import Banner from 'components/Banner'
 import Titulo from 'components/Titulo'
+import Card from 'components/Card'
 
 export default function Inicio() {
     return (
@@ -12,6 +13,7 @@ export default function Inicio() {
             <Titulo>
                 <h1>Um lugar para guardar seus vídeos e filmes!</h1>
             </Titulo>
+            <Card id="1" titulo="card 1" capa="https://avatars.githubusercontent.com/u/63370425?s=400&u=4d1c0f773daed75685e3e5e101086908394f1603&v=4" />
             <Rodape />
         </>
     )

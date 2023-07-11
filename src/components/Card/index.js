e5a4bd1106e29791f4d990de12c568033e7d0a7f
Ravi Silva <ravifelipee@gmail.com>
2023-07-10 23:34:44 -0300

    implementação da função 'adicionarFavorito'

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,15 +1,33 @@
 import React from 'react'
 import styles from "./Card.module.css"
 import iconeFavoritar from "./favoritar.png"
+import iconeDesfavoritar from "./desfavoritar.png"
+import { useFavoritoContext } from 'contextos/Favoritos'
 
 export default function Card({ id, titulo, capa }) {
+    const { favorito, adicionarFavorito } = useFavoritoContext();
+    //Import do Favorito Context (lista de favoritos e a função de adicionar favorito)
+
+    const ehFavorito = favorito.some((fav) => fav.id === id);
+    //Checando se o card em questão está na lista de favoritos
+
+    const icone = !ehFavorito ? iconeFavoritar : iconeDesfavoritar;
+    //Se o card não for Favorito, será exibido o ícone para Favoritar
+    //Se o card for Favorito, será exibido o ícone para Desfavoritar
+
     return (
         <div className={styles.container}>
             <img src={capa} alt={titulo} className={styles.capa} />
             <h2>{titulo}</h2>
-            <img src={iconeFavoritar}
+            <img src={icone}
                 alt='Favoritar Filme'
-                className={styles.favoritar} />
+                className={styles.favoritar}
+                onClick={() => {
+                    adicionarFavorito({ id, titulo, capa })
+                }}
+            //ao clicar no ícone para favoritar, a função de adicionar favoritos foi ativada
+            //foram enviados para a função o "id", "titulo" e a "capa" do card especifico
+            />
         </div>
     )
 }

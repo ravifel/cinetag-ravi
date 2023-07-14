1f244b9984745d4bb1501d664fe6341b212e0b7c
Ravi Silva <ravifelipee@gmail.com>
2023-07-13 23:07:50 -0300

    criação de requisição para ter acesso aos valores da API esternalizada

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -1,11 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Banner from 'components/Banner'
 import Titulo from 'components/Titulo'
 import Card from 'components/Card'
-import videos from 'json/db.json'
 import styles from './Inicio.module.css'
 
 export default function Inicio() {
+    const [videos, setVideos] = useState([]);
+    //criação de uma variavel de videos
+    //setVideos responsavel por atualizar o estado
+
+    useEffect(() => {
+        fetch('https://my-json-server.typicode.com/monicahillman/cinetag-api/videos')
+            .then(resposta => resposta.json())
+            .then(dados => {
+                setVideos(dados)
+            })
+    }, [])
+
+    // -> useEffect
+    //Realiza açõe sapós a renderização da página. 
+    //Requisições sãoi funções assincronas do JavaScript. Pois ela pode demorar um pouco mais do que a renderização da página
+
+    // -> fetch
+    //é a criação da requisição para a API
+    //foram pegos os dados de retorno e converstidos para JSON
+    //depois os dados da lista foram enviados para o "setVideos()"
+    //que por fim fez a atualização dos estados dos videos
+
+    // -> , []
+    //é responsável por fazer que a requisição seja feita só uma vez
+
     return (
         <>
             <Banner imagem="home" />

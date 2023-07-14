bc2e1179c25e493c3f97f90550acc0047e019b0a
Ravi Silva <ravifelipee@gmail.com>
2023-07-13 23:32:12 -0300

    implementação do 'getch' passando parametros para acessar os filmes individualmente

--- src/pages/Player/index.js
+++ src/pages/Player/index.js
@@ -1,23 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from './Player.module.css'
 import Banner from 'components/Banner'
 import Titulo from 'components/Titulo'
 import { useParams } from 'react-router-dom'
-import videos from 'json/db.json'
 import NaoEncontrada from 'pages/NaoEncontrada'
 
 export default function Player() {
+    const [video, setVideo] = useState();
+
     const parametros = useParams();
     //O "useParams" é um hook do React que pega o parametro que está na URL
     //O "ID" que foi passado no parametro da rota
-    const video = videos.find((video) => {
-        return video.id === Number(parametros.id)
-    })
+
+    useEffect(() => {
+        fetch(`https://my-json-server.typicode.com/monicahillman/cinetag-api/videos?id=${parametros.id}`)
+            .then(resposta => resposta.json())
+            .then(dados => {
+                setVideo(...dados)
+            })
+    }, [])
 
     if (!video) {
         return <NaoEncontrada />
     }
-//se o vídeo não for encontrado, as páginas do Player não vão ser carregadas
+    //se o vídeo não for encontrado, as páginas do Player não vão ser carregadas
 
     return (
         <>
